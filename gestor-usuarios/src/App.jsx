c98b4ada51bd861df4dff62c2f55b433a8be72ef
Angel Cruz <angelcruzsanchez126@gmail.com>
2023-11-30 15:05:22 -0600

    Libreria Formik

---
+++ gestor-usuarios/src/App.jsx
@@ -0,0 +1,28 @@
+import './index.css'
+import useFormulario  from './hooks/useFormulario'
+import Input from  './components/Input'
+import Card from './components/Card'
+
+
+function App() {
+  const  [formulario, handleChange] = useFormulario({name: '', lastname: ''})
+  console.log(formulario)
+  return (
+  <Card>
+   <form>
+     <Input 
+     name="name" 
+     label="Nombre" 
+     value={formulario.name} 
+     onChange={handleChange} />
+     <Input 
+     name="lastname"
+     label="Apellido" 
+     value={formulario.lastname} 
+     onChange={handleChange} />
+   </form>
+   </Card>
+  )
+}
+
+export default App
